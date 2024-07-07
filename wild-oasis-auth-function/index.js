51d7165537ab068c86fcaabf899b74e64a7e3f36
Yaman Aswal <yaman@appsquad.com>
2024-07-07 23:06:14 +0530

    INIT

--- wild-oasis-auth-function/index.js
+++ wild-oasis-auth-function/index.js
@@ -2,7 +2,7 @@ export const handler = async (event, context) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'hello world',
+            message: 'hello world aws github actions',
         })
     };
 
