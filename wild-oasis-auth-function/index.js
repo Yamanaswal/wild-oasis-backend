79148ddf0f807f91676649489eb5a1af97c45403
Yaman Aswal <yaman@appsquad.com>
2024-07-08 10:39:03 +0530

    update with dynamodb branch wild-oasis

--- wild-oasis-auth-function/index.js
+++ wild-oasis-auth-function/index.js
@@ -2,14 +2,18 @@ const { createCabin } = require("./src/create_cabin");
 
 module.exports.handler = async function (event, context, callback) {
 
-    switch (event.route_key) {
-        case "create_cabin":
-            const result = await createCabin(event, context);
-            callback(result);
-            break;
+    try {
+        switch (event.route_key) {
+            case "create_cabin":
+                const result = await createCabin(event, context);
+                return result; // Return the result directly
 
-        default:
-            throw new Error(`Unsupported route: "${event.routeKey}"`);
-
+            default:
+                throw new Error(`Unsupported route: "${event.route_key}"`);
+        }
+    } catch (err) {
+        // Handle errors appropriately
+        console.error(err);
+        throw Error("handler error:", err.message);
     }
 };
