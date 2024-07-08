d7eabcf8f00499a1b567f36c897cbb88649119a1
Yaman Aswal <yaman@appsquad.com>
2024-07-08 08:43:02 +0530

    create wild-oasis-branch

--- wild-oasis-auth-function/index.js
+++ wild-oasis-auth-function/index.js
@@ -2,7 +2,9 @@ export const handler = async (event, context) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
+            status: 200,
             message: 'hello world aws github actions',
+            data: {}
         })
     };
 
